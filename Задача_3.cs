decedb872da95f65ff2017fa8385dcad35d3b8e8
Nail Garipov <nailgaripov@mail.ru>
2024-04-08 11:45:17 +0500

    HOME WORK

---
+++ Задача_3.cs
@@ -0,0 +1,16 @@
+﻿// Задайте произвольный массив. Выведете его элементы, 
+// начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+Console.WriteLine("Развернутый массив: ");
+ReversArray(array, array.Length - 1);
+
+static void ReversArray(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write(array[index] + " ");
+        ReversArray(array, index - 1);
+    }
+}
